b8de35a2c18e7ff76390c796c74e0954d3d00751
linxs953 <cakelinxs@outlook.com>
2024-08-07 23:05:05 +0800

    feat: 创建任务接口

--- internal/handler/task/createtaskhandler.go
+++ internal/handler/task/createtaskhandler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
+
 	"lexa-engine/internal/logic/task"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
@@ -18,6 +19,7 @@ func CreateTaskHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 		}
 
 		l := task.NewCreateTaskLogic(r.Context(), svcCtx)
+		req.Author = "linxs"
 		resp, err := l.CreateTask(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
--- internal/logic/task/createtasklogic.go
+++ internal/logic/task/createtasklogic.go
@@ -2,15 +2,16 @@ package task
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/zeromicro/go-zero/core/logx"
 
 	mong "lexa-engine/internal/model/mongo"
 	"lexa-engine/internal/model/mongo/sceneinfo"
 	"lexa-engine/internal/model/mongo/taskinfo"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
-
-	"github.com/google/uuid"
-	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type CreateTaskLogic struct {
@@ -28,19 +29,32 @@ func NewCreateTaskLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Create
 }
 
 func (l *CreateTaskLogic) CreateTask(req *types.CreateTaskDto) (resp *types.CreateTaskResp, err error) {
+	resp = &types.CreateTaskResp{
+		Code:    0,
+		Message: "创建任务成功",
+	}
 	murl := mong.GetMongoUrl(l.svcCtx.Config.Database.Mongo)
 	smod := sceneinfo.NewSceneInfoModel(murl, l.svcCtx.Config.Database.Mongo.UseDb, "SceneInfo")
 	var taskRecord taskinfo.TaskInfo
 	for _, sid := range req.SceneList {
 		var scene *sceneinfo.SceneInfo
-		if scene, err = smod.FindOneBySceneID(context.Background(), sid); err != nil {
+		if scene, err = smod.FindOneBySceneID(context.Background(), sid.SceneId); err != nil {
+			resp.Code = 1
+			resp.Message = err.Error()
 			return
 		}
-		taskRecord.Scenes = append(taskRecord.Scenes, scene.Scene)
+		for i := 0; i < sid.Count; i++ {
+			scene.SceneName = fmt.Sprintf("%s-%d", scene.SceneName, i+1)
+			taskRecord.Scenes = append(taskRecord.Scenes, scene.Scene)
+		}
 	}
+	taskRecord.TaskName = req.TaskName
+	taskRecord.Author = req.Author
 	taskMod := taskinfo.NewTaskInfoModel(murl, l.svcCtx.Config.Database.Mongo.UseDb, "TaskInfo")
 	taskRecord.TaskID = uuid.New().String()
 	if err = taskMod.Insert(context.Background(), &taskRecord); err != nil {
+		resp.Code = 2
+		resp.Message = "创建任务记录失败"
 		return
 	}
 	return
