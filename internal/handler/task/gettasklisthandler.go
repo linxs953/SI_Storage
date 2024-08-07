7f873fc5df6276b059c84956863d55ccb254fc60
linxs953 <cakelinxs@outlook.com>
2024-08-07 15:14:10 +0800

    update:任务列表接口，参数判断

--- internal/handler/task/gettasklisthandler.go
+++ internal/handler/task/gettasklisthandler.go
@@ -2,19 +2,38 @@ package task
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"lexa-engine/internal/logic/task"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func GetTaskListHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.GetTaskListDto
-		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
-			return
+		pageSize := r.URL.Query().Get("pageSize")
+		page := r.URL.Query().Get("page")
+		if pageSize == "" {
+			req.PageSize = 10
+		} else {
+			pageSize, err := strconv.ParseInt(pageSize, 10, 64)
+			if err != nil {
+				req.PageSize = 10
+			}
+			req.PageSize = int(pageSize)
+		}
+
+		if page == "" {
+			req.PageNum = 1
+		} else {
+			page, err := strconv.ParseInt(page, 10, 64)
+			if err != nil {
+				req.PageNum = 1
+			}
+			req.PageNum = int(page)
 		}
 
 		l := task.NewGetTaskListLogic(r.Context(), svcCtx)
