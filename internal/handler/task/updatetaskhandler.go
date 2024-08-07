0b9484de9b24ec92ee824cd57fb67a6f5c08519c
linxs953 <cakelinxs@outlook.com>
2024-08-07 15:41:26 +0800

    feat: 添加更新任务接口

--- internal/handler/task/updatetaskhandler.go
+++ internal/handler/task/updatetaskhandler.go
@@ -1,19 +1,27 @@
 package task
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"lexa-engine/internal/logic/task"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func UpdateTaskHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.UpdateTaskDto
-		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+		// if err := httpx.Parse(r, &req); err != nil {
+		// 	httpx.ErrorCtx(r.Context(), w, err)
+		// 	return
+		// }
+
+		req.TaskId = r.URL.Query().Get("taskId")
+		if req.TaskId == "" {
+			httpx.ErrorCtx(r.Context(), w, fmt.Errorf("taskId is required"))
 			return
 		}
 
--- internal/logic/task/updatetasklogic.go
+++ internal/logic/task/updatetasklogic.go
@@ -3,6 +3,8 @@ package task
 import (
 	"context"
 
+	mgoutil "lexa-engine/internal/model/mongo"
+	"lexa-engine/internal/model/mongo/taskinfo"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
 
@@ -24,7 +26,47 @@ func NewUpdateTaskLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Update
 }
 
 func (l *UpdateTaskLogic) UpdateTask(req *types.UpdateTaskDto) (resp *types.UpdateTaskResp, err error) {
-	// todo: add your logic here and delete this line
+	resp = &types.UpdateTaskResp{
+		Code:    0,
+		Data:    types.TaskInfo{},
+		Message: "success",
+	}
+	murl := mgoutil.GetMongoUrl(l.svcCtx.Config.Database.Mongo)
+	taskMod := taskinfo.NewTaskInfoModel(murl, l.svcCtx.Config.Database.Mongo.UseDb, "TaskInfo")
+	taskInfo, err := taskMod.FindByTaskId(context.Background(), req.TaskId)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		return
+	}
+	if taskInfo == nil {
+		resp.Code = 2
+		resp.Message = "task not found"
+		return
+	}
 
+	// todo: 重新设计入参dto， 更新逻辑再更新
+	taskInfo.TaskName = req.TaskName
+
+	_, err = taskMod.Update(context.Background(), taskInfo)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		return
+	}
+
+	taskInfo, err = taskMod.FindByTaskId(context.Background(), req.TaskId)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		return
+	}
+
+	resp.Data = types.TaskInfo{
+		TaskId:   taskInfo.TaskID,
+		TaskName: taskInfo.TaskName,
+		TaskType: "autoapi",
+		TaskSpec: taskInfo,
+	}
 	return
 }
