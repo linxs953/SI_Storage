eac5c344a5109298f92ba46764a0ab5cfb961270
linxs953 <cakelinxs@outlook.com>
2024-08-07 11:39:26 +0800

    feat: 获取任务信息接口

--- internal/handler/task/gettaskhandler.go
+++ internal/handler/task/gettaskhandler.go
@@ -1,22 +1,25 @@
 package task
 
 import (
+	"errors"
 	"net/http"
 
-	"github.com/zeromicro/go-zero/rest/httpx"
 	"lexa-engine/internal/logic/task"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func GetTaskHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.GetTaskDto
-		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+
+		req.TaskId = r.URL.Query().Get("taskId")
+		if req.TaskId == "" {
+			httpx.ErrorCtx(r.Context(), w, errors.New("taskId为空"))
 			return
 		}
-
 		l := task.NewGetTaskLogic(r.Context(), svcCtx)
 		resp, err := l.GetTask(&req)
 		if err != nil {
--- internal/logic/task/gettasklogic.go
+++ internal/logic/task/gettasklogic.go
@@ -3,6 +3,8 @@ package task
 import (
 	"context"
 
+	mgoutil "lexa-engine/internal/model/mongo"
+	"lexa-engine/internal/model/mongo/taskinfo"
 	"lexa-engine/internal/svc"
 	"lexa-engine/internal/types"
 
@@ -24,7 +26,24 @@ func NewGetTaskLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetTaskLo
 }
 
 func (l *GetTaskLogic) GetTask(req *types.GetTaskDto) (resp *types.GetTaskResp, err error) {
-	// todo: add your logic here and delete this line
-
+	resp = &types.GetTaskResp{
+		Code:    0,
+		Data:    types.TaskInfo{},
+		Message: "success",
+	}
+	murl := mgoutil.GetMongoUrl(l.svcCtx.Config.Database.Mongo)
+	taskMod := taskinfo.NewTaskInfoModel(murl, l.svcCtx.Config.Database.Mongo.UseDb, "TaskInfo")
+	taskInfo, err := taskMod.FindByTaskId(context.Background(), req.TaskId)
+	if err != nil {
+		resp.Code = 1
+		resp.Message = err.Error()
+		return
+	}
+	resp.Data = types.TaskInfo{
+		TaskId:   taskInfo.TaskID,
+		TaskName: taskInfo.TaskName,
+		TaskType: "autoapi",
+		TaskSpec: taskInfo.Scenes,
+	}
 	return
 }
